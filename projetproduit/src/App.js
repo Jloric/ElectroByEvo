84843e9519064216274d0192e7b2d47ebd33e9b5
Dorian <dorianguerry@gmail.com>
2022-07-28 14:14:35 +0200

    Projet basique avec route de test

--- projetproduit/src/App.js
+++ projetproduit/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Test from './Test';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Routes>
+        <Route path="/"  exact element={<Navigate to="/books" replace/>}/>
+        <Route path="/books" exact element={<Test />} />
+        <Route path="/books/add" element = {<Test />}/>
+        <Route path="/books/edit/:id" element = {<Test />}/>
+      </Routes>
+
     </div>
+   
   );
 }
 
