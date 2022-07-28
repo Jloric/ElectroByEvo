811f9fab92f6bdbdeffecc55703dd3f5905463c8
Dorian <dorianguerry@gmail.com>
2022-07-28 14:46:35 +0200

    Projet avec navbar fonctionnelle

--- projetproduit/src/App.js
+++ projetproduit/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Test from './Test';
+import NavbarAdmin from './NavbarAdmin'
+
 function App() {
+
   return (
+  <>
+  <NavbarAdmin/>
     <div className="container">
       <Routes>
         <Route path="/"  exact element={<Navigate to="/books" replace/>}/>
         <Route path="/books" exact element={<Test />} />
-        <Route path="/books/add" element = {<Test />}/>
+        <Route path="/admin/produits/add" element = {<Test />}/>
         <Route path="/books/edit/:id" element = {<Test />}/>
       </Routes>
 
     </div>
-   
+  </>
   );
 }
 
