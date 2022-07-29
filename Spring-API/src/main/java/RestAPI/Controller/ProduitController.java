90e8dc838f7471f9ca4633b754d4420ae0af645d
Minatchy <minatchyjloric@gmail.com>
2022-07-29 15:16:16 +0200

    add search on option by search term or by order or both with notes and prix only

--- Spring-API/src/main/java/RestAPI/Controller/ProduitController.java
+++ Spring-API/src/main/java/RestAPI/Controller/ProduitController.java
@@ -3,8 +3,10 @@ package RestAPI.Controller;
 import RestAPI.DAO.ProduitRepository;
 import RestAPI.DO.ProduitDO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.OrderBy;
 import java.util.List;
 
 @RestController
@@ -13,14 +15,36 @@ public class ProduitController {
     @Autowired
     ProduitRepository produitRepository;
     @GetMapping("/produits")
-    public List<ProduitDO> getAll(@RequestParam(required = false,defaultValue = "") String search){
-        if(search =="") {
+    public List<ProduitDO> getAll(
+            @RequestParam(required = false,name ="search") String search,
+            @RequestParam(required = false,name="sort") String sort)
+    {
+        if(search==null && sort==null) {
             System.err.println("Voici la liste des produits");
             return produitRepository.findAll();
-        }else{
+        }
+        System.out.print(sort);
+        System.out.print(search);
+        if(sort == null){
             System.err.println("Voici la liste des produits");
-            System.err.println("keyword:"+search);
+            System.err.println("keyword:" + search);
             return produitRepository.searchProduit(search);
+        }
+        if(search == null){
+            System.err.println("Voici la liste des produits trié");
+            System.err.println("Sort:" + sort);
+            return produitRepository.findAll(Sort.by(Sort.Direction.ASC,sort));
+        }
+        else{
+            System.err.println("keyword:" + search+"Sort:"+sort);
+            if(sort.equals("note")) {
+                System.err.println("Voici la liste des produits triés par notes et qui correspondent a la recherche");
+
+                return produitRepository.searchProduitSortedByNote(search);
+            }else{
+                System.err.println("Voici la liste des produits triés par prix et qui correspondent a la recherche");
+                return produitRepository.searchProduitSortedByPrix(search);
+            }
         }
     }
     @GetMapping("/produits/{id}")
--- Spring-API/src/main/java/RestAPI/DAO/ProduitRepository.java
+++ Spring-API/src/main/java/RestAPI/DAO/ProduitRepository.java
@@ -4,6 +4,7 @@ import RestAPI.DO.ProduitDO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.OrderBy;
 import java.util.List;
 
 public interface ProduitRepository extends JpaRepository<ProduitDO,Long> {
@@ -12,4 +13,17 @@ public interface ProduitRepository extends JpaRepository<ProduitDO,Long> {
             "Or p.note LIKE CONCAT('%', :search, '%')"+
             "Or p.prix LIKE CONCAT('%', :search, '%')")
     List<ProduitDO> searchProduit(String search);
+    @Query("SELECT p FROM Produit p WHERE " +
+            "p.nom LIKE CONCAT('%',:search, '%')" +
+            "Or p.prix LIKE CONCAT('%', :search, '%')"+
+            "ORDER BY p.note ")
+
+    List<ProduitDO> searchProduitSortedByNote(String search);
+    @Query("SELECT p FROM Produit p WHERE " +
+            "p.nom LIKE CONCAT('%',:search, '%')" +
+            "Or p.prix LIKE CONCAT('%', :search, '%')"+
+            "ORDER BY p.prix ")
+
+    List<ProduitDO> searchProduitSortedByPrix(String search);
+
 }
