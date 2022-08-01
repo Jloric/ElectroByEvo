bc2ee2fb69f1ce5260f4714a6bebd6490d2c2105
Dorian <dorianguerry@gmail.com>
2022-08-01 14:09:56 +0200

    Changement sur le login

--- projetproduit/src/admin/Login.js
+++ projetproduit/src/admin/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navigate} from 'react-router-dom';
 const Login =  () => {
-
+    const [user , setUser] = useState({})
     function connecter()
     {
-        sessionStorage.setItem("token","15645eaze45");
-        window.location.reload();
+        let option ={
+                           method : 'POST',
+                                                    headers : {
+                                                        "content-type" : "application/json"
+                                                    },
+                                                    body : JSON.stringify(user)
+                                                }
+        fetch("http://localhost:8080/api/auth/login", option )
+        .then(token =>{ sessionStorage.setItem("token", token); console.log(token.headers.get("token"))})
 
+       // window.location.reload();
+
+    }
+
+    const changeHandler = ({target})=>
+    {
+        setUser({...user , [target.name] : target.value})
     }
     return (
         <div className="offset-md-4 col-md-4 text-center mt-5">
@@ -14,11 +28,11 @@ const Login =  () => {
             <form>
                 <div className="form-group">
                     <label htmlFor="login" className="form-label">Login</label>
-                    <input id="login" name="login" className="form-control" placeholder="Saisir votre login"/>
+                    <input id="login" name="username" className="form-control" onChange={(e) => changeHandler(e)}placeholder="Saisir votre login"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password" className="form-label">Mot de passe</label>
-                    <input id="password" name="password" className="form-control" placeholder="Saisir votre mot de passe"/>
+                    <input id="password" name="password" className="form-control" onChange={(e) => changeHandler(e)}placeholder="Saisir votre mot de passe"/>
                 </div>
                 <button className="btn btn-primary" onClick={() => connecter()} type="button">Login</button>
             </form>
